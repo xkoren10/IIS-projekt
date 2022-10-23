4efebc8ca113a28096de7c6d8008d2e3979edf92
Matej Hlozek <xhloze02@vutbr.cz>
2022-10-23 23:30:30 +0200

    Hotfix

--- IS/urls.py
+++ IS/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('index.urls')),
     path('admin/', admin.site.urls)
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
