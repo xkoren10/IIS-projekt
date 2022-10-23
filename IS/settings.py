cf5a66690e6176c6a7e53c7791c5f09692ccc929
Matej Hlozek <xhloze02@vutbr.cz>
2022-10-23 23:51:23 +0200

    Hotfix part 4

--- IS/settings.py
+++ IS/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import whitenoise
 #import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
