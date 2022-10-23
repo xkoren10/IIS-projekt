6eb76c9a116b2a76edf35a6db52f9998b38e6945
Matej Hlozek <xhloze02@vutbr.cz>
2022-10-23 23:48:58 +0200

    Hotfix part 3

--- IS/settings.py
+++ IS/settings.py
@@ -59,12 +59,13 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 ROOT_URLCONF = 'IS.urls'
 
 TEMPLATES = [
