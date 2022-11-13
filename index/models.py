bf0e933093b950e7c20ed5ee609f435f5b1b077c
Matej Hlozek <xhloze02@vutbr.cz>
2022-11-13 23:41:29 +0100

    Made last commit work

--- index/models.py
+++ index/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     user_name = models.CharField(max_length=80)
     email = models.CharField(max_length=80)
     password = models.CharField(max_length=80)
-    mod = models.BooleanField
+    mod = models.BooleanField(default=False)
 
 
 class Categories(models.Model):
