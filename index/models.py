5f5e03f6d8ddc414983a38dfe8da16d013208f6e
xkoren10 <xkoren10@stud.fit.vutbr.cz>
2022-11-27 01:34:59 +0100

    Validators and moderation page fix(kinda)

--- index/models.py
+++ index/models.py
@@ -1,6 +1,7 @@
 # This file contains objects for database
 from . import db_logic as db
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from itertools import chain
 # from mptt.models import MPTTModel, TreeForeignKey
 
@@ -25,9 +26,9 @@ class Crop(models.Model):
     crop_name = models.CharField(max_length=80)
     description = models.CharField(max_length=80)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    amount = models.IntegerField()
+    amount = models.PositiveIntegerField()
     origin = models.CharField(max_length=80)
-    crop_year = models.IntegerField(null=True)
+    crop_year = models.PositiveIntegerField(null=True)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     price_type = models.CharField(max_length=5,
                                   choices=[
@@ -43,8 +44,8 @@ class Order(models.Model):
 
     ordered_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ordered_by')
     farmer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='farmer')
-    total_price = models.FloatField()
-    amount = models.IntegerField()
+    total_price = models.FloatField(MinValueValidator(0.0))
+    amount = models.PositiveIntegerField()
     crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
     state = models.CharField(max_length=10,
                              choices=[("pen", "pending"), ("rej", "rejected"), ("con", "confirmed")],
@@ -57,8 +58,8 @@ class Harvest(models.Model):
     place = models.CharField(max_length=80)
     description = models.CharField(max_length=80)
     crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
-    max_occupancy = models.IntegerField(default=0)
-    current_occupancy = models.IntegerField(default=0)
+    max_occupancy = models.PositiveIntegerField(default=0)
+    current_occupancy = models.PositiveIntegerField(default=0)
     farmer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='harvest_farmer')
     attendees = models.ManyToManyField(User)
 
@@ -67,7 +68,7 @@ class Review(models.Model):
 
     short_desc = models.CharField(max_length=80)
     long_desc = models.CharField(max_length=200)
-    stars = models.IntegerField()
+    stars = models.PositiveIntegerField(MaxValueValidator(5))
     crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
     reviewed_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
